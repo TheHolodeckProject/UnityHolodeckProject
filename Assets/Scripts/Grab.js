ec66d5aff473de0a615dcbdffcf82441cb4c7346
kevroy314 <kevin.horecka@gmail.com>
2014-06-05 15:20:30 -0500

    Added GUI and Grab
    -Added Loader scene for initial configuration and loading.
    -Added grab functionality
    
    Note: When I tried to test using a standard character controller, the
    inputs were all wonky. I'm a little concerned that we've broken
    something without knowing it when it comes to the project input
    settings...

--- Assets/Scripts/Grab.js
+++ Assets/Scripts/Grab.js
@@ -4,82 +4,54 @@
 	//Transfer the part where it counts stimuli to the Stimuli script
 	//Get it to output starting and finish position
 
+#pragma strict
+
 //Imports tools to use generic lists
 import System.Collections.Generic;
 
 //Initializes variables
 var grabRange = 1.0; //Not sure what GrabRange does, as you can grab it from any distance no matter what you set it at
-var holdDistance = .2; //How far away the object is held from your character
-var grabbedObjects = new List.<String>(); //Initializes a list that will hold the names of grabbed objects
+var holdDistance = 0.1; //How far away the object is held from your character
+var grabKeys : List.<KeyCode> = new List.<KeyCode>();
+private var resetParent = new GameObject(); //How far away the object is held from your character
+private var grabbedObject = new GameObject();;
+
 static var resetflag = false; //Initiates a flag that will change to true once all the objects have been touched
 
-//Defines the 2 possible values of GrabState
+//Defines the 3 possible values of GrabState
 enum GrabState {Free, Hold};
+
 //Sets private variables to be used within this scipt
 private var rigid : Rigidbody = null;
 private var grabState : GrabState = 0; //Sets the default grabstate variable
 private var stimNumber = 5.0;
-var objectforward = Vector3(0,0,0); 
-
+ 
 //Runs this function every frame
 function FixedUpdate () {
-	//If you're NOT holding the object
-    if(grabState == GrabState.Free) {
-			//If you press the joystick
-        if (Input.GetButtonDown("Trigger"))	{
-//            Debug.Log("Trigger pressed");
-        //Defines the hitInfo variable using the RaycastHit function (?)
-        	var hitInfo : RaycastHit;
-        	//If the ray hits an object (?)
-            if(Physics.Raycast(transform.position, transform.forward, hitInfo)) {
-								//If the object the ray hits has a rigidbody (?)
-		        if(hitInfo.rigidbody) {
-		        	//Defines the rigid variable and puts it in hitInfo.rigidbody
-         	     	rigid = hitInfo.rigidbody;
-         	     	
-//Displays a message if the grabbedObjects list already contains the object
-if(grabbedObjects.Contains(hitInfo.collider.gameObject.name)) {
- //Debug.Log(hitInfo.collider.gameObject.name + " is already in the list");
- }
-else 
-{
-        	     	//If grabbedObjects doesn't already contain the item, adds it
-         	     	grabbedObjects.Add(hitInfo.collider.gameObject.name);
-//Debug.Log("Put " + hitInfo.collider.gameObject.name + " into the list");
- }
- 
- //Checks if the list of grabbedObjects contains all the objects
- if (grabbedObjects.Count >= stimNumber && !resetflag) {
- resetflag = true;
-Debug.Log("Resetting objects...");
- }
-					//Declares that you're holding the variable now
-          		 	grabState = GrabState.Hold;
-                 	}
-            	}
-        	}
-    	}
-    //If you ARE holding the object
-    else if(grabState == GrabState.Hold) {
-        //Changes the position of the object you're holding to be holdDistance in front of you
-        rigid.MovePosition(transform.position + transform.forward * holdDistance);
-
-       //rigid.MovePosition(transform.Translate(transform.position,transform.rotation))
-
-//        rigid.MovePosition(transform.position + this.transform.forward * holdDistance;
-        //objectforward = transform.localRotation * new Vector3(0,1,1);
-//        rigid.MovePosition(transform.position + (transform.rotation * objectforward); 
-        //Vector3 newpos = transform.position + (transform.localRotation)
-        //rigid.MovePosition(transform.position + new Vector3(transform.forward.x,transform.forward.y,transform.forward.z));
-       //Debug.Log(transform.position);
-       //Debug.Log(transform.rotation);
-        //If the left mouse is not being pressed
-        if(Input.GetButtonUp("Trigger")){
-        //You stop holding the object
-        grabState = GrabState.Free;
- //       Debug.Log("Trigger released");
-    }
+	//If you're NOT holding the object		
+	var grabEventDown = false;
+	var grabEventUp = false;
+	for(var i = 0; i < grabKeys.Count;i++){
+		grabEventUp = grabEventUp || Input.GetKeyUp(grabKeys[i]);
+		grabEventDown = grabEventDown || Input.GetKeyDown(grabKeys[i]);
 	}
+	
+	
+	if(grabbedObject==null&&grabEventDown) {
+		//Defines the hitInfo variable using the RaycastHit function (?)
+		var hitInfo : RaycastHit;
+		//If the ray hits an object (?)
+		if(Physics.Raycast(transform.position, transform.forward, hitInfo)) {
+			grabbedObject = hitInfo.rigidbody.gameObject;
+			resetParent = grabbedObject.transform.parent.gameObject;
+			grabbedObject.transform.parent = this.transform;
+		}
+	}
+    //If you ARE holding the object
+    if(grabbedObject!=null && grabEventUp){
+   		grabbedObject.transform.parent = resetParent.transform;
+   		grabbedObject = null;
+    }
 }
 //Makes the script also execute in edit mode
 @script ExecuteInEditMode()
---
+++ Assets/Scripts/LoadGUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGUI : MonoBehaviour {
+
+	string comPortFieldString = "COM1";
+	string subjectIdentifierString = "1234567890";
+
+	void OnGUI() {
+		// Make a background box
+		GUI.BeginGroup (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 50, 500, 500));
+		//GUI.Box (new Rect (10,10,500,500), "");
+		GUI.Label (new Rect (20, 20, 300, 30), "Haptics Com Port");
+		comPortFieldString = GUI.TextField (new Rect (20, 50, 120, 20), comPortFieldString);
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if (GUI.Button (new Rect (20,80,120,20), "Test Haptics")) {
+			string comPort = comPortFieldString;
+			testHaptics();
+		}
+
+		GUI.Label (new Rect (200, 20, 300, 30), "Subject Identifier");
+		subjectIdentifierString = GUI.TextField (new Rect (200, 50, 120, 20), subjectIdentifierString);
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if (GUI.Button (new Rect (200,80,120,20), "Enter Simulation")) {
+			string comPort = comPortFieldString;
+			Application.LoadLevel (1);
+		}
+		GUI.EndGroup ();
+	}
+
+	void testHaptics(){
+		//INSERT TEST CODE FOR HAPTICS HERE!
+	}
+}
