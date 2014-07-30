d8ad71817db0dffbe1edd54c80be9758b4b118c5
Aldi <aldis.sipolins@gmail.com>
2014-07-30 00:51:26 -0500

    Spatial N-Back progress
    Figuring out how to generate and place objects at runtime. Transparency
    seems to be working. Starting to think occlusion might be an issue,
    though...

--- Assets/Scripts/NBackGenerate.cs
+++ Assets/Scripts/NBackGenerate.cs
@@ -3,11 +3,30 @@ using System.Collections;
 
 public class NBackGenerate : MonoBehaviour {
 
+    //Declare variables to be used in the script
     public GameObject cube;
+    private Vector3 cubePosition;
+    private float stimNumber;
 
 	// Use this for initialization
 	void Start () {
-        Instantiate(cube, new Vector3(1, 1, 5), Quaternion.identity);
+
+
+        //For every cube in the grid
+        for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                for (int z = 0; z < 3; z++)
+                {
+                    stimNumber = x + y + z;
+                    Debug.Log(stimNumber);
+                    //Defines the position of the cube in the series
+                    cubePosition = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z + z);
+                    Instantiate(cube, cubePosition, Quaternion.identity);
+                }
+            }
+        }
 	}
 	
 	// Update is called once per frame
