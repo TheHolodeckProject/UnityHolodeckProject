bfa5947ac6e7e80154d95fe10d41cbec1dc098b2
Aldi <aldis.sipolins@gmail.com>
2014-07-29 19:27:37 -0500

    Holodeck Environment Progress 3
    Added new patterns, got them lines up pretty nice. Trying out generating
    cube skeleton prefabs at runtime.

--- Assets/Scripts/NBackGenerate.cs
+++ Assets/Scripts/NBackGenerate.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NBackStimuli : MonoBehaviour {
+public class NBackGenerate : MonoBehaviour {
+
+    public GameObject cube;
 
 	// Use this for initialization
 	void Start () {
-	
+        Instantiate(cube, new Vector3(1, 1, 5), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
