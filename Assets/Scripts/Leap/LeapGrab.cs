1b5bb381bf714ccfa7d25f80f4dfa0447c798d9c
HolodeckPC <theholodeckproject@gmail.com>
2014-09-24 16:26:24 -0500

    LeapStretch
    Finally got a scene running where you can stretch out a virtual cube.
    Also deleted some older scenes I didn't think we were using anymore.

--- Assets/Scripts/Leap/LeapGrab.cs
+++ Assets/Scripts/Leap/LeapGrab.cs
@@ -1,133 +1,133 @@
-﻿/******************************************************************************\
-* Copyright (C) Leap Motion, Inc. 2011-2014.                                   *
-* Leap Motion proprietary. Licensed under Apache 2.0                           *
-* Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
-\******************************************************************************/
+﻿///******************************************************************************\
+//* Copyright (C) Leap Motion, Inc. 2011-2014.                                   *
+//* Leap Motion proprietary. Licensed under Apache 2.0                           *
+//* Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
+//\******************************************************************************/
 
-using UnityEngine;
-using System.Collections;
-using Leap;
+//using UnityEngine;
+//using System.Collections;
+//using Leap;
 
-// Leap Motion hand script that detects pinches and grabs the
-// closest rigidbody with a spring force if it's within a given range.
-public class LeapGrab : MonoBehaviour
-{
+//// Leap Motion hand script that detects pinches and grabs the
+//// closest rigidbody with a spring force if it's within a given range.
+//public class LeapGrab : MonoBehaviour
+//{
 
-    private const float TRIGGER_DISTANCE_RATIO = 0.7f;
+//    private const float TRIGGER_DISTANCE_RATIO = 0.7f;
 
-    private bool pinching_;
-    private Collider grabbed_;
-    private Collider rightpinched;
-    //private Collider rightthumbtipcollider;
-    private Finger rightpinchfinger;
-    private Bone rightpinchbone;
+//    private bool pinching_;
+//    private Collider grabbed_;
+//    private Collider rightpinched;
+//    //private Collider rightthumbtipcollider;
+//    private Finger rightpinchfinger;
+//    private Bone rightpinchbone;
 
-    void Start()
-    {
-        pinching_ = false;
-        grabbed_ = null;
-    }
+//    void Start()
+//    {
+//        pinching_ = false;
+//        grabbed_ = null;
+//    }
 
-    void OnPinch(Vector3 pinch_position)
-    {
-        pinching_ = true;
+//    void OnPinch(Vector3 pinch_position)
+//    {
+//        pinching_ = true;
 
-        // Check if we pinched a movable object and grab the closest one that's not part of the hand.
-        //Collider[] close_things = Physics.OverlapSphere(pinch_position, magnetDistance);
-        //Vector3 distance = new Vector3(magnetDistance, 0.0f, 0.0f);
+//        // Check if we pinched a movable object and grab the closest one that's not part of the hand.
+//        //Collider[] close_things = Physics.OverlapSphere(pinch_position, magnetDistance);
+//        //Vector3 distance = new Vector3(magnetDistance, 0.0f, 0.0f);
 
-        //for (int j = 0; j < close_things.Length; ++j)
-        //{
-        //    Vector3 new_distance = pinch_position - close_things[j].transform.position;
-        //    if (close_things[j].rigidbody != null && new_distance.magnitude < distance.magnitude &&
-        //        !close_things[j].transform.IsChildOf(transform))
-        //    {
-        //        grabbed_ = close_things[j];
-        //        distance = new_distance;
-        //    }
-        //}
-    }
+//        //for (int j = 0; j < close_things.Length; ++j)
+//        //{
+//        //    Vector3 new_distance = pinch_position - close_things[j].transform.position;
+//        //    if (close_things[j].rigidbody != null && new_distance.magnitude < distance.magnitude &&
+//        //        !close_things[j].transform.IsChildOf(transform))
+//        //    {
+//        //        grabbed_ = close_things[j];
+//        //        distance = new_distance;
+//        //    }
+//        //}
+//    }
 
-    void OnRelease()
-    {
-        grabbed_ = null;
-        pinching_ = false;
-    }
+//    void OnRelease()
+//    {
+//        grabbed_ = null;
+//        pinching_ = false;
+//    }
 
-    void Update()
-    {
-        HandModel hand_model = GetComponent<HandModel>();
-        Hand leap_hand = hand_model.GetLeapHand();
+//    void Update()
+//    {
+//        HandModel hand_model = GetComponent<HandModel>();
+//        Hand leap_hand = hand_model.GetLeapHand();
 
-        if (leap_hand == null)
-            return;
+//        if (leap_hand == null)
+//            return;
 
-        // Scale trigger distance by thumb proximal bone length.
-        Vector leap_thumb_tip = leap_hand.Fingers[0].TipPosition;
-        float proximal_length = leap_hand.Fingers[0].Bone(Bone.BoneType.TYPE_PROXIMAL).Length;
-        float trigger_distance = proximal_length * TRIGGER_DISTANCE_RATIO;
+//        // Scale trigger distance by thumb proximal bone length.
+//        Vector leap_thumb_tip = leap_hand.Fingers[0].TipPosition;
+//        float proximal_length = leap_hand.Fingers[0].Bone(Bone.BoneType.TYPE_PROXIMAL).Length;
+//        float trigger_distance = proximal_length * TRIGGER_DISTANCE_RATIO;
         
-        //Finds the tip of the thumb, which is its own game object
-        GameObject rightthumbtip = GameObject.Find("MagneticPinchHand(Clone)/thumb/bone1");
-        //If the thumb is in the scene
-        if (rightthumbtip != null)
-           // Collider rightthumbtip = 
-            //Checks if its collider is hitting anything with the tag "StrechPoint"
-        //    if rightthumbtip.collider.
+//        //Finds the tip of the thumb, which is its own game object
+//        GameObject rightthumbtip = GameObject.Find("MagneticPinchHand(Clone)/thumb/bone1");
+//        //If the thumb is in the scene
+//        if (rightthumbtip != null)
+//           // Collider rightthumbtip = 
+//            //Checks if its collider is hitting anything with the tag "StrechPoint"
+//        //    if rightthumbtip.collider.
            
 
-        //else
-        //    Debug.Log("Thumb's not here");
+//        //else
+//        //    Debug.Log("Thumb's not here");
     
 
-        //Collider thumbcollider = leap_hand.Fingers[1].
+//        //Collider thumbcollider = leap_hand.Fingers[1].
 
-        // Check if thumb is colliding with a strechable point
-        //if 
-        //leap_hand.
+//        // Check if thumb is colliding with a strechable point
+//        //if 
+//        //leap_hand.
 
-        //!!! Could maybe make this more efficient by not doing it for the thumb
-        for (int i = 1; i < HandModel.NUM_FINGERS; ++i)
-        {
-            Finger finger = leap_hand.Fingers[i];
-
-
-            //!!! Could maybe make this more efficient by only looking at the fingertip bone
-            for (int j = 0; j < FingerModel.NUM_BONES; ++j)
-            {
-                Vector leap_joint_position = finger.Bone((Bone.BoneType)j).NextJoint;
-                if (leap_joint_position.DistanceTo(leap_thumb_tip) < trigger_distance)
-                {
-                    //rightpinchbone = finger.Bone((Bone.BoneType)j);
-                    rightpinchfinger = leap_hand.Fingers[i];
-                    //!!! Could maybe make this more efficient by not going through the rest of the fingers if one of them is pinching
-                }
-            }
-        }
-
-        //If pinching, check if the two fingers pinching are colliding with one of the colliders
-            //Collider rightpinched = collider;
+//        //!!! Could maybe make this more efficient by not doing it for the thumb
+//        for (int i = 1; i < HandModel.NUM_FINGERS; ++i)
+//        {
+//            Finger finger = leap_hand.Fingers[i];
 
 
-            ////Converts the Leap position vector to a unity Vector3
-            //Vector3 pinchposition = UnityVectorExtension.ToUnityScaled(rightpinchfinger.TipPosition);
-            //if (Physics.CheckSphere(pinchposition, 0.01f))
-            //    Debug.Log("Pinching something...");
-            ////Collision.Equals(rightpinch)
-    }
+//            //!!! Could maybe make this more efficient by only looking at the fingertip bone
+//            for (int j = 0; j < FingerModel.NUM_BONES; ++j)
+//            {
+//                Vector leap_joint_position = finger.Bone((Bone.BoneType)j).NextJoint;
+//                if (leap_joint_position.DistanceTo(leap_thumb_tip) < trigger_distance)
+//                {
+//                    //rightpinchbone = finger.Bone((Bone.BoneType)j);
+//                    rightpinchfinger = leap_hand.Fingers[i];
+//                    //!!! Could maybe make this more efficient by not going through the rest of the fingers if one of them is pinching
+//                }
+//            }
+//        }
 
-        //Vector3 pinch_position = hand_model.fingers[0].GetTipPosition();
+//        //If pinching, check if the two fingers pinching are colliding with one of the colliders
+//            //Collider rightpinched = collider;
 
-        // Only change state if it's different.
-        //if (trigger_pinch && !pinching_)
-        //    OnPinch(pinch_position);
-        //else if (!trigger_pinch && pinching_)
-        //    OnRelease();
 
-        //// Accelerate what we are grabbing toward the pinch.
-        //if (grabbed_ != null)
-        //{
-        //    Vector3 distance = pinch_position - grabbed_.transform.position;
-        //    grabbed_.rigidbody.AddForce(forceSpringConstant * distance);
-        //}
-    }
+//            ////Converts the Leap position vector to a unity Vector3
+//            //Vector3 pinchposition = UnityVectorExtension.ToUnityScaled(rightpinchfinger.TipPosition);
+//            //if (Physics.CheckSphere(pinchposition, 0.01f))
+//            //    Debug.Log("Pinching something...");
+//            ////Collision.Equals(rightpinch)
+//    }
+
+//        //Vector3 pinch_position = hand_model.fingers[0].GetTipPosition();
+
+//        // Only change state if it's different.
+//        //if (trigger_pinch && !pinching_)
+//        //    OnPinch(pinch_position);
+//        //else if (!trigger_pinch && pinching_)
+//        //    OnRelease();
+
+//        //// Accelerate what we are grabbing toward the pinch.
+//        //if (grabbed_ != null)
+//        //{
+//        //    Vector3 distance = pinch_position - grabbed_.transform.position;
+//        //    grabbed_.rigidbody.AddForce(forceSpringConstant * distance);
+//        //}
+//    }
--- Assets/Scripts/Stimulus/Stretch.cs
+++ Assets/Scripts/Stimulus/Stretch.cs
@@ -2,88 +2,93 @@
 using System.Collections;
 using Leap;
 
+//THINGS TO TRY
+//1) Using the Leap pinch gesture from the MagneticPinch script instead of colliders. Might be more stable?
+// Would also make so you cant pinch and wipe your hand through an object to grab. We had the same issue with KinectGrab and solved it by detecting a grab and then looking for a rising edge.
+
+//2) Instead of comparing to the position and scale when grabbed, try manipulating based on position and scale from the previous frame?
+
+// !!! While stretching, the thumb can leave the area of the collider. Not sure why. 
 public class Stretch : MonoBehaviour
 {
-    private bool fingertouch = false;
-    private bool thumbtouch = false;
-    private Vector3 thumbposition;
-    private Vector3 prevposition;
+    private bool fingertouch;
+    private bool thumbtouch;
     private GameObject cube;
-    private float prevx;
-    private Vector3 startposition;
-    
+    private GameObject thumb;
+    private Vector3 thumbCurrentPosition;
+    private Vector3 thumbStartPosition;
+    private Vector3 cubeStartScale;
+    private Vector3 cubeStartPosition;
+
     void Start()
     {
+        fingertouch = false;
+        thumbtouch = false;
         cube = GameObject.Find("StrechableCube");
     }
 
-    //Whenever something starts colliding with the cube, check if it's the thumb or the index finger.
+    //Whenever something starts colliding with the cube, checks if it's the thumb or the index finger.
     void OnCollisionEnter(Collision col)
     {
-        //For everything colliding with the cube collider
+        //Checks the list of all the things colliding with the cube to see if it contains the index fingertip and the thumb fingertip
         foreach (ContactPoint contact in col.contacts)
         {
-            //Checks the name of the thing colliding with it
-            switch (contact.otherCollider.gameObject.transform.parent.name)
+            //If the tip of the index finger is touching
+            if (contact.otherCollider.gameObject.transform.name == "bone3" && contact.otherCollider.gameObject.transform.parent.name == "index")
             {
-                case "index":
-                    fingertouch = true;
-                    Debug.Log("Index touched");
-                    break;
-                case "thumb":
-                    thumbtouch = true;
-                    Debug.Log("Thumb touched");
-                    Vector3 startposition = GameObject.Find("MagneticPinchHand(Clone)/thumb/bone1").transform.position;
-                    break;
-
-                   
+                Debug.Log("FingerTip Touching" + this.gameObject.name);
+                fingertouch = true;
+            }
+            // If the thumb tip is touching
+            if (contact.otherCollider.gameObject.transform.name == "bone3" && contact.otherCollider.gameObject.transform.parent.name == "thumb")
+            {
+                thumbtouch = true;
+                //Defines the thumb game object. Doing it this way means it works with any hand
+                thumb = contact.otherCollider.gameObject;
+                Debug.Log("Thumb Touching" + this.gameObject.name);
+                //Grabs the position and scale of the thumb when it entered the object
+                thumbStartPosition = thumb.transform.position;
+                //Also grabs the scale and position of the cube when it entered the object
+                cubeStartScale = cube.transform.localScale;
+                cubeStartPosition = cube.transform.position;
             }
         }
     }
 
     void OnCollisionExit(Collision col)
     {
+     //Whenever something STOPS colliding with the cube, checks if it's the thumb or the index finger.
         foreach (ContactPoint contact in col.contacts)
         {
-            switch (contact.otherCollider.gameObject.transform.parent.name)
-            {
-                case "index":
-                    fingertouch = false;
-                    //Debug.Log("Index no longer touching");
-                    break;
-                case "thumb":
-                    thumbtouch = false;
-                    //Debug.Log("Thumb no longer touching");
-                    break;
-            }
+            if (contact.otherCollider.gameObject.transform.name == "bone3" && contact.otherCollider.gameObject.transform.parent.name == "index")
+                fingertouch = false;
+            if (contact.otherCollider.gameObject.transform.name == "bone3" && contact.otherCollider.gameObject.transform.parent.name == "thumb")
+                thumbtouch = false;
         }
     }
 
     void Update()
     {
-        Vector3 thumbposition = GameObject.Find("MagneticPinchHand(Clone)/thumb/bone1").transform.position;
-       
         //If the finger and thumb are grabbing
         if (fingertouch == true && thumbtouch == true)
         {
-            Debug.Log("Pinching");
-            // Finds the position of the thumb.
-                //Makes the actual cube a child of the thumb
-            thumbposition = GameObject.Find("MagneticPinchHand(Clone)/thumb/bone1").transform.position;
-            Debug.Log(thumbposition);
+            //Gets the position of the thumb
+            Vector3 thumbCurrentPosition = thumb.transform.position;
+            //Calculates differences between current position and starting position. This will be used to modify the cube's scale and position
+            Vector3 difference = thumbCurrentPosition - thumbStartPosition;
+            //Debug.Log("Cube Position: " + cube.transform.position);
+            //Debug.Log("Cube Scale: " + cube.transform.localScale);
+            //Debug.Log("Difference: " + difference);
+
+            //Applies different transformations depending on which corner is being grabbed
+            switch (this.gameObject.transform.name)
+            {
+                case "CornerColliderBackBottomLeft":
+                    cube.transform.localScale = new Vector3(cubeStartScale.x + difference.x, cubeStartScale.y - difference.y, cubeStartScale.z);
+                    cube.transform.position = new Vector3(cubeStartPosition.x + difference.x / 2, cubeStartPosition.y + difference.y / 2, cubeStartPosition.z + difference.z / 2);
+                    break;
+            }
         }
-        Vector3 difference = startposition - thumbposition;
-
-        //Debug.Log("Difference = " + difference);
-        //Debug.Log();
-//        Debug.Log("Difference = " + difference);
-
-
-
-        cube.transform.position = cube.transform.position + difference;
-
-
     }
-        //Stores the previous position so we can track if it's changed
-        //prevposition = thumbposition;
 }
+
