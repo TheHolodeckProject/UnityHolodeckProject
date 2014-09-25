d8cd9f6f391aac5caf6c674d1083893dda1aa9c4
Aldi <aldis.sipolins@gmail.com>
2014-09-25 00:30:22 -0500

    LeapStretch Improvements
    Made it so you can now stretch from any corner of the cube. Stretching
    with both hands even seems to work. This is totally going to work.

--- Assets/Scripts/Stimulus/Stretch.cs
+++ Assets/Scripts/Stimulus/Stretch.cs
@@ -2,23 +2,22 @@
 using System.Collections;
 using Leap;
 
-//THINGS TO TRY
-//1) Using the Leap pinch gesture from the MagneticPinch script instead of colliders. Might be more stable?
+//TO ADD
+//1) Moving & Rotating - If the thumb and index of a single hand grab different colliders on the cube, then you can move and rotate - how to do?
+//2) Using the Leap pinch gesture from the MagneticPinch script instead of colliders. Might be more stable?
 // Would also make so you cant pinch and wipe your hand through an object to grab. We had the same issue with KinectGrab and solved it by detecting a grab and then looking for a rising edge.
+//3) Generate random transparent cube within certain parameters.
+//4) Add visual cue to let you know when you're touching a collider
 
-//2) Instead of comparing to the position and scale when grabbed, try manipulating based on position and scale from the previous frame?
-
-// !!! While stretching, the thumb can leave the area of the collider. Not sure why. 
 public class Stretch : MonoBehaviour
 {
     private bool fingertouch;
     private bool thumbtouch;
     private GameObject cube;
     private GameObject thumb;
-    private Vector3 thumbCurrentPosition;
-    private Vector3 thumbStartPosition;
-    private Vector3 cubeStartScale;
-    private Vector3 cubeStartPosition;
+    private Vector3 cubeCurrentPosition;
+    private Vector3 cubeCurrentScale;
+    private Vector3 thumbPreviousPosition;
 
     void Start()
     {
@@ -46,11 +45,8 @@ public class Stretch : MonoBehaviour
                 //Defines the thumb game object. Doing it this way means it works with any hand
                 thumb = contact.otherCollider.gameObject;
                 Debug.Log("Thumb Touching" + this.gameObject.name);
-                //Grabs the position and scale of the thumb when it entered the object
-                thumbStartPosition = thumb.transform.position;
-                //Also grabs the scale and position of the cube when it entered the object
-                cubeStartScale = cube.transform.localScale;
-                cubeStartPosition = cube.transform.position;
+                //Initializes a variable to simplify things later on in the script
+                thumbPreviousPosition = thumb.transform.position;
             }
         }
     }
@@ -72,22 +68,45 @@ public class Stretch : MonoBehaviour
         //If the finger and thumb are grabbing
         if (fingertouch == true && thumbtouch == true)
         {
-            //Gets the position of the thumb
-            Vector3 thumbCurrentPosition = thumb.transform.position;
-            //Calculates differences between current position and starting position. This will be used to modify the cube's scale and position
-            Vector3 difference = thumbCurrentPosition - thumbStartPosition;
-            //Debug.Log("Cube Position: " + cube.transform.position);
-            //Debug.Log("Cube Scale: " + cube.transform.localScale);
-            //Debug.Log("Difference: " + difference);
-
-            //Applies different transformations depending on which corner is being grabbed
+            //Defines the current position and scale of the cube
+            Vector3 currentCubePosition = cube.transform.position;
+            Vector3 currentCubeScale = cube.transform.localScale;
+            //Defines how much the thumb has moved since the last frame. This position difference will be used to stretch the cube      
+            Vector3 thumbDifference = thumb.transform.position - thumbPreviousPosition;
+            // ??? Why does adding Vector3 to the start of the next line cause an error?
+            thumbPreviousPosition = thumb.transform.position;
+            //Moves the cube's position based on how the thumb's moved
+            cube.transform.position = new Vector3(currentCubePosition.x + thumbDifference.x / 2, currentCubePosition.y + thumbDifference.y / 2, currentCubePosition.z + thumbDifference.z / 2);
+            //Applies different transformations to scale depending on which corner is being grabbed
+            //I'm sure there's a beautiful elegant way to do this, but this way seems to work for now
             switch (this.gameObject.transform.name)
             {
+                case "CornerColliderFrontBottomLeft":
+                    cube.transform.localScale = new Vector3(currentCubeScale.x + thumbDifference.x, currentCubeScale.y - thumbDifference.y, currentCubeScale.z + thumbDifference.z);
+                    break;
+                case "CornerColliderFrontBottomRight":
+                    cube.transform.localScale = new Vector3(currentCubeScale.x - thumbDifference.x, currentCubeScale.y - thumbDifference.y, currentCubeScale.z + thumbDifference.z);
+                    break;
+                case "CornerColliderFrontTopLeft":
+                    cube.transform.localScale = new Vector3(currentCubeScale.x + thumbDifference.x, currentCubeScale.y + thumbDifference.y, currentCubeScale.z + thumbDifference.z);
+                    break;
+                case "CornerColliderFrontTopRight":
+                    cube.transform.localScale = new Vector3(currentCubeScale.x - thumbDifference.x, currentCubeScale.y + thumbDifference.y, currentCubeScale.z + thumbDifference.z);
+                    break;
                 case "CornerColliderBackBottomLeft":
-                    cube.transform.localScale = new Vector3(cubeStartScale.x + difference.x, cubeStartScale.y - difference.y, cubeStartScale.z);
-                    cube.transform.position = new Vector3(cubeStartPosition.x + difference.x / 2, cubeStartPosition.y + difference.y / 2, cubeStartPosition.z + difference.z / 2);
+                    cube.transform.localScale = new Vector3(currentCubeScale.x + thumbDifference.x, currentCubeScale.y - thumbDifference.y, currentCubeScale.z - thumbDifference.z);
+                    break;
+                case "CornerColliderBackBottomRight":
+                    cube.transform.localScale = new Vector3(currentCubeScale.x - thumbDifference.x, currentCubeScale.y - thumbDifference.y, currentCubeScale.z - thumbDifference.z);
+                    break;
+                case "CornerColliderBackTopLeft":
+                    cube.transform.localScale = new Vector3(currentCubeScale.x + thumbDifference.x, currentCubeScale.y + thumbDifference.y, currentCubeScale.z - thumbDifference.z);
+                    break;
+                case "CornerColliderBackTopRight":
+                    cube.transform.localScale = new Vector3(currentCubeScale.x - thumbDifference.x, currentCubeScale.y + thumbDifference.y, currentCubeScale.z - thumbDifference.z);
                     break;
             }
+         
         }
     }
 }
