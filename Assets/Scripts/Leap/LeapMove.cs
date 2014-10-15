25235b5916ad71b262630fcf1a50d75056fa96ea
Aldi <aldis.sipolins@gmail.com>
2014-10-15 00:44:04 -0500

    TableTopTask Changes

--- Assets/Scripts/Leap/LeapMove.cs
+++ Assets/Scripts/Leap/LeapMove.cs
@@ -1,23 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Leap;
 
 public class LeapMove : MonoBehaviour
 {
     enum State { Idle, Moving, EndMove }
     private State currentState;
-    private GameObject cube;
     private bool triggerMove;
-    public static bool moving;
+    public bool moving;
     private bool fingerTouch;
     private bool thumbTouch;
     private Vector3 middlePosition;
     private Vector3 middlePrevPosition;
     private GameObject moveManager;
+    // ??? For what I'm using this for, should I be using a transform or a gameobject
+    private Transform cube;
     // Use this for initialization
     void Start()
     {
         currentState = State.Idle;
-        cube = GameObject.Find("StretchableCube");
         triggerMove = false;
         moving = false;
     }
@@ -28,41 +29,61 @@ public class LeapMove : MonoBehaviour
         switch (currentState)
         {
             case State.Idle:
-                    DetectFingerCollisions();
-                    if (triggerMove)
-                        currentState = OnMove();
-                    break;
-            
+                DetectFingerCollisions();
+                if (triggerMove)
+                    currentState = OnMove();
+                break;
+
             case State.Moving:
-                Debug.Log("Moving");
                 DetectFingerCollisions();
                 if (triggerMove)
                     Move();
-                else                
+                else
                     currentState = EndMove();
                 break;
         }
     }
 
-        void DetectFingerCollisions()
+    void DetectFingerCollisions()
+    // ??? Would it be more efficient to just find the closest cube like we did in iPosition?
     {
+        cube = null;
         fingerTouch = false;
         thumbTouch = false;
-        foreach (Transform child in cube.transform)
+        //Gets the thumb position
+        Vector3 thumbTipPosition = GameObject.Find(this.name + "/thumb/bone3").transform.position;
+        // Checks a tiny sphere collider around the thumb
+        Collider[] thumbTouching = Physics.OverlapSphere(thumbTipPosition, .01f);
+        //For everything in the radius around the thumbtip
+        for (int j = 0; j < thumbTouching.Length; ++j)
         {
-            if (child.gameObject.GetComponent<DetectTouch>().thumbTouch)
+            //If it's colliding with one of the child colliders of the StretchableCube
+            if (thumbTouching[j].transform.parent.name == "StretchableCube(Clone)")
             {
-                thumbTouch = true;
-                //Debug.Log("Thumb touching" + child.name);
-                continue;
+                cube = thumbTouching[j].transform.parent.transform;
+                break;
             }
-            if (child.gameObject.GetComponent<DetectTouch>().fingerTouch)
+        }
+        //If it found a cube    
+        if (cube != null)
+        {
+            //For everything the cube is touching
+            foreach (Transform child in cube)
             {
-                fingerTouch = true;
-                //Debug.Log("Finger touching" + child.name);
-                continue;
+                //Check if the thumb and index finger are touching different colliders
+                if (child.gameObject.GetComponent<DetectTouch>().thumbTouch)
+                {
+                    thumbTouch = true;
+                    //Debug.Log("Thumb touching" + child.name);
+                    continue;
+                }
+                if (child.gameObject.GetComponent<DetectTouch>().fingerTouch)
+                {
+                    fingerTouch = true;
+                    //Debug.Log("Finger touching" + child.name);
+                    continue;
+                }
             }
-
         }
         if (fingerTouch && thumbTouch)
             triggerMove = true;
@@ -82,7 +103,7 @@ public class LeapMove : MonoBehaviour
             //Calculates the difference between where the middle point is now and where it was a frame ago
             Vector3 diffPosition = middlePrevPosition - middlePosition;
             //Applies that difference to the grabbed cube
-            cube.transform.position = cube.transform.position - diffPosition;
+            cube.position = cube.transform.position - diffPosition;
             //Logs the middle position to be used as a reference next frame
             middlePrevPosition = middlePosition;
         }
@@ -99,11 +120,13 @@ public class LeapMove : MonoBehaviour
         //Previous position gets subtracted from current position in later frames, so we need a previous position to start with
         // ??? Why does adding Vector3 to this make is inacessible by Move?
         middlePrevPosition = Vector3.Lerp(thumbPosition, fingerPosition, .5f);
+        //Moves the grabbed object a little closer to the middle position when you grab it
+        cube.position = Vector3.MoveTowards(cube.position, middlePrevPosition, .005f);
         return State.Moving;
     }
 
     State EndMove()
-    {  
+    {
         moving = false;
         if (!fingerTouch)
             Debug.Log("Ending move because the finger stopped touching");
--- Assets/Scripts/Stimulus/StretchTaskStateMachine.cs
+++ Assets/Scripts/Stimulus/StretchTaskStateMachine.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using Leap;
+
 
 public class StretchTaskStateMachine : MonoBehaviour
 {
-    public Transform cube;
+    public GameObject cube;
+    private GameObject[] stimuli;
     public int totalTrials = 3;
     public int numberOfStim = 1;
-    private GameObject[] stimuli;
+    public float cubeScale = .5f;
     private Vector3[] stimLocations;
     private int currentTrial;
     private Controller controller;
+    public Vector3 randomBoundsP0 = new Vector3(-.3f, -42.1f, -.1f);
+    public Vector3 randomBoundsP1 = new Vector3(.3f, -42.3f, .1f);
     enum State { TrialStart = 0, TrialIdle, TrialEnd, TaskEnd };
     private State currentState;
 
@@ -21,16 +26,16 @@ public class StretchTaskStateMachine : MonoBehaviour
         stimuli = new GameObject[numberOfStim];
         currentState = State.TrialStart;
         currentTrial = 0;
-        //ADDED
-       controller = new Controller();
+        cube.tag = "Loggable";
 
-       InteractionBox iBox = GetInteractionBox();
-       GameObject interactionBoxCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-       interactionBoxCube.transform.position = new Vector3(iBox.Center.ToUnityScaled().x, iBox.Center.ToUnityScaled().y, iBox.Center.ToUnityScaled().z);
-       Vector v = new Vector(iBox.Width, iBox.Height, iBox.Depth);
-       interactionBoxCube.transform.localScale = new Vector3(v.ToUnityScaled().x, v.ToUnityScaled().y, v.ToUnityScaled().z);
-       
-       Debug.Log(" x: " + iBox.Center.ToUnityScaled().x + " y:" + iBox.Center.ToUnityScaled().y + " z:" + iBox.Center.ToUnityScaled().z + " width: " + v.ToUnityScaled().x + " height: " + v.ToUnityScaled().y + " depth: " + v.ToUnityScaled().z);
+
+        // ??? Not working
+        //InteractionBox iBox = GetInteractionBox();
+       //GameObject interactionBoxCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+       //interactionBoxCube.transform.position = new Vector3(iBox.Center.ToUnityScaled().x, iBox.Center.ToUnityScaled().y, iBox.Center.ToUnityScaled().z);
+       //Vector v = new Vector(iBox.Width, iBox.Height, iBox.Depth);
+       //interactionBoxCube.transform.localScale = new Vector3(v.ToUnityScaled().x, v.ToUnityScaled().y, v.ToUnityScaled().z);
+       //Debug.Log(" x: " + iBox.Center.ToUnityScaled().x + " y:" + iBox.Center.ToUnityScaled().y + " z:" + iBox.Center.ToUnityScaled().z + " width: " + v.ToUnityScaled().x + " height: " + v.ToUnityScaled().y + " depth: " + v.ToUnityScaled().z);
     }
 
     // Update is called once per frame
@@ -45,7 +50,6 @@ public class StretchTaskStateMachine : MonoBehaviour
                 break;
 
             case State.TrialIdle:
-                Debug.Log("Recalling");
                 break;
         }
     }
@@ -55,21 +59,111 @@ public class StretchTaskStateMachine : MonoBehaviour
         return GameObject.Find("HandController").GetComponent<HandController>().latestFrame.InteractionBox;
     }
 
-    // ??? How to access frame.LeapInteractionBox? I want to use that as the limits of where to generate stimuli
     void GenerateStimLocations()
     {
         for (int i = 0; i < numberOfStim; i++)
         {
-            stimLocations[i] = new Vector3 (Random.Range(-.3f, .3f), Random.Range(-42f, -42.4f), Random.Range(-.15f, .15f));
+            stimLocations[i] = new Vector3 (Random.Range(-.3f, .3f), Random.Range(-42.1f, -42.3f), Random.Range(-.1f, .1f));
           //  stimSizes[i] = new Vector3(LeapStretchStateMachine.MinCubeSize LeapStretchStateMachine.MaxCubeSize)
+
+            //    //Position stimuli randomly according to settings (no overlaps)
+            //    List<Rect> overlapCheckList = new List<Rect>();
+            //    int retries = 100;
+            //    for (int i = 0; i < numberOfStim; i++)
+            //    {
+            //        stimLocations[i] = new Vector3(
+            //            Random.Range(randomBoundsP0.x > randomBoundsP1.x ? randomBoundsP1.x : randomBoundsP0.x,
+            //                      randomBoundsP0.x < randomBoundsP1.x ? randomBoundsP1.x : randomBoundsP0.x),
+            //            Random.Range(randomBoundsP0.y > randomBoundsP1.y ? randomBoundsP1.y : randomBoundsP0.y,
+            //                      randomBoundsP0.y < randomBoundsP1.y ? randomBoundsP1.y : randomBoundsP0.y),
+            //            Random.Range(randomBoundsP0.z > randomBoundsP1.z ? randomBoundsP1.z : randomBoundsP0.z,
+            //                      randomBoundsP0.z < randomBoundsP1.z ? randomBoundsP1.z : randomBoundsP0.z));
+            //        //Check for overlapping boxes and regenerate box location if overlap occurs
+            //        Rect newBox = new Rect(stimuli[i].x,
+            //                               stimuli[i].z,
+            //                               stimuli[i].GetComponentInChildren<MeshFilter>().mesh.bounds.size.x * stimuli[i].transform.localScale.x,
+            //                               stimuli[i].GetComponentInChildren<MeshFilter>().mesh.bounds.size.z * stimuli[i].transform.localScale.z);
+            //        if (boxesOverlapArray(newBox, overlapCheckList))
+            //        {
+            //            retries--;
+            //            if (retries <= 0)
+            //            {
+            //                //Force quit and overlap to prevent hang
+            //               // retries = numRetries;
+            //                overlapCheckList.Add(newBox);
+            //                Debug.Log("Unable to find proper placement of object. Too many objects or incorrect mesh bounds?");
+            //            }
+            //            else i--;
+            //        }
+            //        else
+            //        {
+            //         //   retries = numRetries;
+            //            overlapCheckList.Add(newBox);
         }
     }
     private void GenerateStimuli()
     {
         for (int i = 0; i < numberOfStim; i++)
         {
-            Debug.Log("Generating Stimuli");
-            stimuli[i] = Instantiate(cube, stimLocations[i],Random.rotation) as GameObject;
+            cube = Instantiate(Resources.Load("StretchableCube")) as GameObject;
+            stimuli[i] = cube;
+            stimuli[i].transform.position = stimLocations[i];
+            stimuli[i].transform.rotation = Random.rotation;
         }
+    }
+    //void generateRandomPositions()
+    //{
+    //    //Position stimuli randomly according to settings (no overlaps)
+    //    List<Rect> overlapCheckList = new List<Rect>();
+    //    int retries = 100;
+    //    for (int i = 0; i < numberOfStim; i++)
+    //    {
+    //        stimLocations[i] = new Vector3(
+    //            Random.Range(randomBoundsP0.x > randomBoundsP1.x ? randomBoundsP1.x : randomBoundsP0.x,
+    //                      randomBoundsP0.x < randomBoundsP1.x ? randomBoundsP1.x : randomBoundsP0.x),
+    //            Random.Range(randomBoundsP0.y > randomBoundsP1.y ? randomBoundsP1.y : randomBoundsP0.y,
+    //                      randomBoundsP0.y < randomBoundsP1.y ? randomBoundsP1.y : randomBoundsP0.y),
+    //            Random.Range(randomBoundsP0.z > randomBoundsP1.z ? randomBoundsP1.z : randomBoundsP0.z,
+    //                      randomBoundsP0.z < randomBoundsP1.z ? randomBoundsP1.z : randomBoundsP0.z));
+    //        //Check for overlapping boxes and regenerate box location if overlap occurs
+    //        Rect newBox = new Rect(stimuli[i].x,
+    //                               stimuli[i].z,
+    //                               stimuli[i].GetComponentInChildren<MeshFilter>().mesh.bounds.size.x * stimuli[i].transform.localScale.x,
+    //                               stimuli[i].GetComponentInChildren<MeshFilter>().mesh.bounds.size.z * stimuli[i].transform.localScale.z);
+    //        if (boxesOverlapArray(newBox, overlapCheckList))
+    //        {
+    //            retries--;
+    //            if (retries <= 0)
+    //            {
+    //                //Force quit and overlap to prevent hang
+    //               // retries = numRetries;
+    //                overlapCheckList.Add(newBox);
+    //                Debug.Log("Unable to find proper placement of object. Too many objects or incorrect mesh bounds?");
+    //            }
+    //            else i--;
+    //        }
+    //        else
+    //        {
+    //         //   retries = numRetries;
+    //            overlapCheckList.Add(newBox);
+    //        }
+    //    }
+    //}
+
+    bool boxesOverlapArray(Rect box, List<Rect> boxArray)
+    {
+        for (var i = 0; i < boxArray.Count; i++)
+            if (boxesOverlap(box, boxArray[i])) return true;
+        return false;
+    }
+
+    //Reference: http://gamemath.com/2011/09/detecting-whether-two-boxes-overlap/
+    bool boxesOverlap(Rect box0, Rect box1)
+    {
+        if (box0.x + box0.width < box1.x) return false;
+        if (box0.x > box1.x + box1.width) return false;
+        if (box0.y + box0.height < box1.y) return false;
+        if (box0.y > box1.y + box1.height) return false;
+        return true;
     }
 }
